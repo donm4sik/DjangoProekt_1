11e48a25de18a57f2b7942bbc2968af102d52c33
donm4sik <donm4sik1988@gmail.com>
2024-01-12 14:46:10 +0300

    Создал новые поля, сделал ряд миграций!

--- apps/todo/models.py
+++ apps/todo/models.py
@@ -1,13 +1,21 @@
 from django.db import models
 
+from apps.todo.models_helpers import create_default_description
+
 class Task(models.Model):
-    title = models.CharField(max_length=75)
+    title = models.CharField(
+        max_length=75,
+        default="DEFAULT TITLE!!!",
+        unique_for_date="date_started"
+    )
     description = models.TextField(
         max_length=1500,
-        validators="task details"
-    )
-    date_started = models.DateField()
-    deadline = models.DateField()
+        verbose_name="task details",
+        default=create_default_description
+
+                                   )
+    date_started = models.DateField(help_text="День когда задача должна начаться")
+    deadline = models.DateField(help_text="День когда задача должна быть выполнена")
     created_at = models.DateTimeField(
         auto_now_add=True,
     )
